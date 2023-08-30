39147d329d9dc9ac9fceaa8bab2a248bdb9a8be9
Itz_KiwiSap_ <programmingjesse@gmail.com>
2023-08-30 16:27:35 +0200

    Added getPlugin method to plugin class

--- per-group-drops/src/main/java/nl/itz_kiwisap_/spigot/pergroupdrops/KiwiPerGroupDropsPlugin.java
+++ per-group-drops/src/main/java/nl/itz_kiwisap_/spigot/pergroupdrops/KiwiPerGroupDropsPlugin.java
@@ -18,6 +18,11 @@ public final class KiwiPerGroupDropsPlugin extends JavaPlugin implements KiwiPer
     }
 
     @Override
+    public @NotNull JavaPlugin getPlugin() {
+        return this;
+    }
+
+    @Override
     public @NotNull PacketInterceptorHandler getPacketInterceptorHandler() {
         return this.instance.getPacketInterceptorHandler();
     }
