18aa522fa76daa40a8a5dc5aa5b7d87971fa489e
Itz_KiwiSap_ <programmingjesse@gmail.com>
2024-01-02 11:18:37 +0100

    Made shapeless recipe matching work

--- custom-recipes/src/main/java/nl/itz_kiwisap_/spigot/customrecipes/recipe/wrapper/ShapelessRecipeWrapper.java
+++ custom-recipes/src/main/java/nl/itz_kiwisap_/spigot/customrecipes/recipe/wrapper/ShapelessRecipeWrapper.java
@@ -1,7 +1,11 @@
 package nl.itz_kiwisap_.spigot.customrecipes.recipe.wrapper;
 
+import nl.itz_kiwisap_.spigot.customrecipes.recipe.ingredient.RecipeIngredient;
 import nl.itz_kiwisap_.spigot.customrecipes.recipe.types.ShapelessRecipe;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 final class ShapelessRecipeWrapper extends RecipeWrapper<ShapelessRecipe> {
 
@@ -11,6 +15,25 @@ final class ShapelessRecipeWrapper extends RecipeWrapper<ShapelessRecipe> {
 
     @Override
     public boolean matches(ItemStack[][] matrix) {
-        return false;
+        List<RecipeIngredient> clonedIngredients = new ArrayList<>(this.recipe.ingredients());
+
+        int matched = 0;
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                ItemStack item = matrix[y][x];
+                if (item == null || item.getType().isAir()) continue;
+
+                for (int i = 0; i < clonedIngredients.size(); i++) {
+                    RecipeIngredient ingredient = clonedIngredients.get(i);
+                    if (ingredient.test(item)) {
+                        clonedIngredients.remove(i);
+                        matched++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return matched == this.recipe.ingredients().size();
     }
 }
